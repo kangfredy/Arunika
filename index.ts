c985d4b7facd1066b0de8a263315eb9f6e2f95b6
fredy <fredy.setiawan@appfuxion.com>
2024-09-04 13:51:28 +0800

    add swagger body parser

--- index.ts
+++ index.ts
@@ -5,12 +5,17 @@ import { apiRoute } from "./src/routes";
 import { globalConfig } from "./src/config";
 
 const app = express();
+app.use(express.json());
+//define routes
 apiRoute(app);
+//define port
 const port = process.env.APP_PORT ?? 3000;
+//enable swagger
 if (globalConfig.ENABLE_SWAGGER) {
   app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 }
 
+//start server
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at port ${port} 🫡`);
 });
